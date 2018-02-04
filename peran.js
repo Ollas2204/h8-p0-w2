aac3ba3acb2a3050de5deae82a627f7cc3f60fab
Fitri Sakinah <fitrisakinah@Fitris-MacBook.local>
2018-02-04 18:33:10 +0700

    tugas 3

---
+++ peran.js
@@ -0,0 +1,19 @@
+var nama = 'Mikael';
+var peran = '';
+if(nama==='' && peran=='ksatria'); {
+  console.log("nama harus diisi!");
+}
+  if(nama==='Mikael' && peran===''); {
+  console.log("Halo Mikael, Pilih peranmu untuk memulai game!");
+  }
+  if(nama==='Nina' && peran==='Ksatria'); {
+    console.log("Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!");
+  }
+  if(nama==='Danu' && peran==='Tabib'); {
+    console.log("Selamat datang di Dunia Proxytia, Danu ." + "Halo Tabib Danu, kamu akan membantu temanmu yang terluka.");
+  }
+  if (nama==='Zero' && peran==='Penyihir'); {
+    console.log("Selamat datang di Dunia Proxytia, Zero");
+  if (nama==='Zero' && peran==='Penyihir');
+      console.log("Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!");
+  }
