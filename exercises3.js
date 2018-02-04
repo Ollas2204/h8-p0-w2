6d0648929df5cbf1dfa62e716426c0a64f5c468a
Fitri Sakinah <fitrisakinah@Fitris-MacBook.local>
2018-02-04 20:10:00 +0700

    update

--- exercises3.js
+++ exercises3.js
@@ -1,26 +1,26 @@
 var nama = 'Mikael';
 var peran = '';
-if (nama!=='')  {
+if (nama==='')  {
 console.log("nama harus diisi!");
 }
- else if (nama!=='Mikael' && peran!=='') {
+ else if (nama==='Mikael' && peran!=='') {
  console.log("Halo Mikael, Pilih peranmu untuk memulai game!");
  }
- if(nama!=='' && peran!=='Ksatria') {
+ if(nama==='' && peran!=='Ksatria') {
     console.log("Selamat datang di Dunia Proxytia, + nama");
   }
-  else if(nama!=='' && peran!=='Ksatria') {
+  else if(nama==='' && peran!=='Ksatria') {
  console.log("Halo Ksatria + nama +", "kamu dapat menyerang dengan senjatamu!");
   }
-  if(nama!=='' && peran!=='Tabib') {
+  if(nama==='' && peran!=='Tabib') {
     console.log("Selamat datang di Dunia Proxytia, + nama");
   }
-  else if (nama!=='' && peran!=='Tabib'){
+  else if (nama==='' && peran!=='Tabib'){
  console.log("Halo Tabib + nama +", "kamu akan membantu temanmu yang terluka")
   }
   if (nama!=='' && peran!=='Penyihir') {
     console.log("Selamat datang di Dunia Proxytia, + nama");
   }
-  else if(nama!=='' && peran!=='Penyihir') {
+  else if(nama==='' && peran!=='Penyihir') {
  console.log("Halo Penyihir + nama +" , "ciptakan keajaiban yang membantu kemenanganmu!");
   }
