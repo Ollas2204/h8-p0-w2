cf60edbd70e7d39b4171f2be67b9251965ce411e
“Your <fitrisakinah17@gmail.com>
2018-02-04 21:08:36 +0700

    tai

--- exercises3.js
+++ exercises3.js
@@ -8,13 +8,14 @@ console.log("nama harus diisi!");
  }
  else if(nama==='' && peran==='Ksatria') {
     console.log("Selamat datang di Dunia Proxytia,"+ nama);
- console.log("Halo Ksatria" + nama +, "kamu dapat menyerang dengan senjatamu!");
+
+ console.log("Halo Ksatria + nama +", "kamu dapat menyerang dengan senjatamu!");
   }
   else if(nama==='' && peran==='Tabib') {
     console.log("Selamat datang di Dunia Proxytia", + nama);
- console.log("Halo Tabib" + nama +, "kamu akan membantu temanmu yang terluka")
+ console.log("Halo Tabib + nama +", "kamu akan membantu temanmu yang terluka")
   }
-  else(nama==='' && peran==='Penyihir') {
+  else if(nama==='' && peran==='Penyihir') {
     console.log("Selamat datang di Dunia Proxytia", + nama);
- console.log("Halo Penyihir" + nama + , "ciptakan keajaiban yang membantu kemenanganmu!");
+ console.log("Halo Penyihir + nama + ", "ciptakan keajaiban yang membantu kemenanganmu!");
   }
