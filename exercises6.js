5bb997b691fc06e213efc562a4beda827225aa58
“Ollas2204” <fitrisakinah17@gmail.com>
2018-02-06 14:10:47 +0700

    nambah euy

---
+++ exercises6.js
@@ -0,0 +1,12 @@
+//LOOPING PERTAMA
+while(looping <= 20 ) {
+  console.log(looping + ' I love coding' );
+  looping += 2
+}
+
+//LOOPING KEDUA
+var looping = 20 ;
+while(looping >= 2 ) {
+  console.log(looping + ' I will become fullstack developer' );
+  looping -= 2
+}
