0a398393fea8e36d638788afcd19a06346f8eaf6
“Ollas2204” <fitrisakinah17@gmail.com>
2018-02-07 17:15:56 +0700

    perbaikan

--- exercises4.js
+++ exercises4.js
@@ -1,19 +1,22 @@
 var tanggal = 5 ;
-var bulan = 9 ;
+var bulan = 13 ;
 var tahun = 2018;
+var bulanPurnama = '';
 
 switch(bulan) {
-  case 1:   { console.log(tanggal + ' januari ' + tahun); break; }
-  case 2:   { console.log(tanggal + ' februari ' + tahun); break; }
-  case 3:   { console.log(tanggal + ' maret ' + tahun); break; }
-  case 4:   { console.log(tanggal + ' april ' + tahun); break; }
-  case 5:   { console.log(tanggal + ' mei ' + tahun); break; }
-  case 6:   { console.log(tanggal + ' juni ' + tahun); break; }
-  case 7:   { console.log(tanggal + ' juli ' + tahun); break; }
-  case 8:   { console.log(tanggal + ' agustus ' + tahun); break; }
-  case 9:   { console.log(tanggal + ' september ' + tahun); break; }
-  case 10:   { console.log(tanggal + ' oktober ' + tahun); break; }
-  case 11:   { console.log(tanggal + ' november ' + tahun); break; }
-  case 12:   { console.log(tanggal + ' desember ' + tahun); break; }
+  case 1:   { bulanPurnama =' januari '; break; }
+  case 2:   { bulanPurnama = 'februari'; break; }
+  case 3:   { bulanPurnama =' maret '; break; }
+  case 4:   { bulanPurnama =' april '; break; }
+  case 5:   { bulanPurnama =' mei '; break; }
+  case 6:   { bulanPurnama =' juni '; break; }
+  case 7:   { bulanPurnama =' july '; break; }
+  case 8:   { bulanPurnama =' agustus '; break; }
+  case 9:   { bulanPurnama =' september '; break; }
+  case 10:  { bulanPurnama =' oktober '; break; }
+  case 11:  { bulanPurnama =' november '; break; }
+  case 12:  { bulanPurnama =' desember '; break; }
   default:  { console.log('tolong isi bulan'); }
 }
+
+console.log(tanggal +  ' ' +bulanPurnama + tahun)
