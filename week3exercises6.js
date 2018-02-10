6686183f71f1602c459c380bf334606e3197d071
“Ollas2204” <fitrisakinah17@gmail.com>
2018-02-10 18:44:04 +0700

    mayan

--- week3exercises6.js
+++ week3exercises6.js
@@ -1,15 +1,26 @@
 function angkaPalindrome(num) {
   // you can only write your code here!
-  var tambah= '';
-  for (var i = String(num).length-1 ; i <= 0; i--) {
-      console.log(String(num)[i]);
+num = num +1
+
+while (true) {
+
+  let balik = String(num).split('').reverse().join('')
+
+  if (num === Number(balik)) {
+    break
+  }else {
+  num++
   }
 
 }
 
+return num
+
+}
+
 // TEST CASES
-// console.log(angkaPalindrome(8)); // 9
-// console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11
 console.log(angkaPalindrome(117)); // 121
 console.log(angkaPalindrome(175)); // 181
-// console.log(angkaPalindrome(1000)); // 1001
+console.log(angkaPalindrome(1000)); // 1001
