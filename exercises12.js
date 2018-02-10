27121409925b3ea0b7280fd77dc14aee5f266c1c
“Ollas2204” <fitrisakinah17@gmail.com>
2018-02-10 16:56:20 +0700

    yeye

--- exercises12.js
+++ exercises12.js
@@ -1,16 +1,28 @@
-var menit = 65;
 function konversiMenit(menit) {
   // you can only write your code here!
-  var jam = menit/60 ;
+  var jam = menit / 60;
   var pembulatan = Math.floor(jam);
-  var minute = menit % 60;
+  var mm = menit % 60 ;
 
-    var n = minute.toString();
-      if( n < 10) {
-        n = '0' + n;
-      }
+  // if (mm < 10) {
+  //   return pembulatan +':0' +mm
+  // } else {
+  //   return pembulatan +':' +mm
+  // }
+//========================================
+    // if (mm.toString().length === 1 ) {
+    //   return pembulatan +':0' +mm
+    // } else {
+    //   return pembulatan +':' +mm
+    // }
+//=========================================
+return mm < 10 ? pembulatan +':0' +mm : pembulatan +':' +mm
 
-      return pembulatan + ':'+n;
 }
-var hasilKonversi = konversiMenit(menit);
- console.log(hasilKonversi)
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
