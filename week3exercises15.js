4dfceda546d18cc45ce4f0388a32645bb89b02c6
“Ollas2204” <fitrisakinah17@gmail.com>
2018-02-23 13:41:22 +0700

    nambah

--- week3exercises15.js
+++ week3exercises15.js
@@ -1,5 +1,22 @@
 function groupAnimals(animals) {
   // you can only write your code here!
+  var result = []
+    var hit = []
+    var sortAnimals = animals.sort()
+    for (var i = 0; i < animals.length; i++) {
+      hit.push(animals[i])
+      if (i !== animals.length - 1) {
+        if (animals[i][0] !== (animals[i+1][0])) {
+          result.push(hit)
+          hit = []
+        }
+      }
+      else {
+        result.push(hit)
+        hit = []
+      }
+    }
+    return result
 }
 
 // TEST CASES
